c9cb91164edb700f4fd20a5ec506300555931da2
Sara012003 <sarag4063@gmail.com>
2025-01-17 15:42:16 -0300

    Desktop Model Design, Main Index

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,25 @@ module.exports = {
       maxWidth: {
         '100px': '100px', // Agrega un max-width de 100px
       },
+      colors:{
+        gold: '#FFD700', // Color dorado personalizado
+      },
       margin: {
         '50px': '100px', // Agrega un margen de 3000px
+        fontFamily:{
+          playfair: ['"Playfair Display"', 'serif'],
+
+        },
       },
     },
-  },
+  }
+  
+  ,backgroundImage: {
+    Background_Dekstop :"url('/public/Bg.jpg')"
+  ,
+  Background_Movil:"url('/public/Bg_Movil.jpg')",
+  Background_Div:"url('/public/Background.jpeg')",
+
   plugins: [],
+}
 };
